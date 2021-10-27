3a84fd3b4b564074ca3c3ce99adc0c45c8f6274e
KhairulAlamNahid <profreelancer10@gmail.com>
2021-10-28 01:28:03 +0600

    serrse

---
+++ js/script.js
@@ -0,0 +1,161 @@
+$(function () {
+    // header search box
+    $('.search-btn').click(function () {
+        $('.search').toggleClass("show-search");
+    })
+
+    // banner slider js
+    $('#banner').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        autoplay: true,
+        speed: 700,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // responsive: [{
+        //         breakpoint: 1024,
+        //         settings: {
+        //             slidesToShow: 3,
+        //             slidesToScroll: 3,
+        //             infinite: true,
+        //             dots: true
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 600,
+        //         settings: {
+        //             slidesToShow: 2,
+        //             slidesToScroll: 2
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 480,
+        //         settings: {
+        //             slidesToShow: 1,
+        //             slidesToScroll: 1
+        //         }
+        //     }
+        // ]
+    });
+
+    // counter up js
+    $('.counter').counterUp({
+        delay: 20,
+        time: 1200
+    });
+
+    // works slider js
+    $('.wroks-main').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        autoplay: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.works-btm',
+        // responsive: [{
+        //         breakpoint: 1024,
+        //         settings: {
+        //             slidesToShow: 3,
+        //             slidesToScroll: 3,
+        //             infinite: true,
+        //             dots: true
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 600,
+        //         settings: {
+        //             slidesToShow: 2,
+        //             slidesToScroll: 2
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 480,
+        //         settings: {
+        //             slidesToShow: 1,
+        //             slidesToScroll: 1
+        //         }
+        //     }
+        // ]
+    });
+    // works bottom slider js
+    $('.works-btm').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        autoplay: true,
+        speed: 1000,
+        slidesToShow: 5,
+        asNavFor: '.wroks-main',
+        centerMode: true,
+        centerPadding: '0',
+        slidesToScroll: 1,
+        // responsive: [{
+        //         breakpoint: 1024,
+        //         settings: {
+        //             slidesToShow: 3,
+        //             slidesToScroll: 3,
+        //             infinite: true,
+        //             dots: true
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 600,
+        //         settings: {
+        //             slidesToShow: 2,
+        //             slidesToScroll: 2
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 480,
+        //         settings: {
+        //             slidesToShow: 1,
+        //             slidesToScroll: 1
+        //         }
+        //     }
+        // ]
+    });
+
+    // video slider js
+    $('.video-slider').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        autoplay: true,
+        speed: 1000,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.works-btm',
+        prevArrow: '<i class="fa fa-angle-left icon left"></i>',
+        nextArrow: '<i class="fa fa-angle-right icon right"></i>',
+        // responsive: [{
+        //         breakpoint: 1024,
+        //         settings: {
+        //             slidesToShow: 3,
+        //             slidesToScroll: 3,
+        //             infinite: true,
+        //             dots: true
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 600,
+        //         settings: {
+        //             slidesToShow: 2,
+        //             slidesToScroll: 2
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 480,
+        //         settings: {
+        //             slidesToShow: 1,
+        //             slidesToScroll: 1
+        //         }
+        //     }
+        // ]
+    });
+
+    // venobox js
+    $('.venobox').venobox();
+})
