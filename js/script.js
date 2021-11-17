304f7717b83805995484d5030b2ae439e275dd7d
KhairulAlamNahid <profreelancer10@gmail.com>
2021-11-17 20:05:41 +0600

    serser

--- js/script.js
+++ js/script.js
@@ -158,4 +158,43 @@ $(function () {
 
     // venobox js
     $('.venobox').venobox();
+
+    // event details slider js
+    $('.lts-main').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        autoplay: true,
+        speed: 500,
+        fade:true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.works-btm',
+        prevArrow: '<i class="fa fa-angle-left icon left"></i>',
+        nextArrow: '<i class="fa fa-angle-right icon right"></i>',
+        // responsive: [{
+        //         breakpoint: 1024,
+        //         settings: {
+        //             slidesToShow: 3,
+        //             slidesToScroll: 3,
+        //             infinite: true,
+        //             dots: true
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 600,
+        //         settings: {
+        //             slidesToShow: 2,
+        //             slidesToScroll: 2
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 480,
+        //         settings: {
+        //             slidesToShow: 1,
+        //             slidesToScroll: 1
+        //         }
+        //     }
+        // ]
+    });
 })
